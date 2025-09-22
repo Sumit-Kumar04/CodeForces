1550b3a6fe7829eaa00b796a16d5721ce9d71cd4
Sumit-Kumar04 <sumitkumar4102004b@gmail.com>
2025-09-22 21:47:52 +0530

    B_Lasers

---
+++ B_Lasers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class B_Lasers{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int  n=sc.nextInt();
+            int  m=sc.nextInt();
+            int  x=sc.nextInt();
+            int  y=sc.nextInt();
+          
+            for(int j=0;j<n;j++){
+                sc.nextInt();
+            }
+            
+            for(int j=0;j<m;j++){
+                sc.nextInt();
+            }
+            System.out.println(m+n);
+        }
+    }
+}
