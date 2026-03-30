90f7b32f0b4df9cc679ee248202590105d879bc3
Sumit-Kumar04 <sumitkumar4102004b@gmail.com>
2026-03-30 14:15:30 +0530

    import java.util.Scanner ;

---
+++ A_Jagged_Swaps.java
@@ -0,0 +1,32 @@
+import java.util.Scanner ;
+
+public class A_Jagged_Swaps{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+           
+            int minArr=Integer.MAX_VALUE;
+            int minstart=Integer.MAX_VALUE;
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+
+            }
+
+            minstart=arr[0];
+            for(int i=1;i<n;i++){
+                minArr=Math.min(minArr,arr[i]);
+            }
+
+            if(minArr<minstart){
+                System.out.println("NO");
+            }else{
+                System.out.println("YES");
+            }
+
+
+        }
+    }
+}
